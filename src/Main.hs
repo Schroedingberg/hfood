0d27297e5da9a6abb5247bfaed4b7b2a2fd9f794
Schroedingberg <aaronrebmann@gmail.com>
2016-09-27 16:53:32 +0200

    Define a basic Food data type

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
-module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello world"
+data Food  = Food {
+  name :: String,
+  -- Energy shall be only given in kJ. Showing everything in kcal might be made available as an option
+  energy :: Float,
+  -- Macronutrients shall be provided as fractions (so <= 1)
+  protein :: Float,
+  fat :: Float,
+  carbs :: Float
+                  } deriving (Show)
