f49ba2cd05f32591795655fe3e16102fe94ae267
Schroedingberg <aaronrebmann@gmail.com>
2016-10-14 09:54:38 +0200

    Move definition of food data type into separate file

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,2 @@
-
-data Food  = Food {
-  name :: String,
-  -- Energy shall be only given in kJ. Showing everything in kcal might be made available as an option
-  energy :: Float,
-  -- Macronutrients shall be provided as fractions (so <= 1)
-  protein :: Float,
-  fat :: Float,
-  carbs :: Float
-                  } deriving (Show)
+import System.IO  
+import Control.Monad
